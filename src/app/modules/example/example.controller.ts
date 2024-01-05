93ae95d5a57a714ce01a12e0660755026a538343
MohonSaha <practicewithmohon@gmail.com>
2024-01-05 11:50:55 +0600

    use catchAsync and avoid code repeatation

--- src/app/modules/example/example.controller.ts
+++ src/app/modules/example/example.controller.ts
@@ -1,63 +1,46 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
 import { UserService } from './example.service';
 import { UserValidations } from './example.validation';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { user } = req.body;
+const createUser = catchAsync(async (req, res) => {
+  const { user } = req.body;
 
-    // validate data through zod
-    const zodParsedData = UserValidations.userValidationSchema.parse(user);
-    const result = await UserService.createUserIntoDB(zodParsedData);
+  // validate data through zod
+  const zodParsedData = UserValidations.userValidationSchema.parse(user);
+  const result = await UserService.createUserIntoDB(zodParsedData);
 
-    sendResponse(res, {
-      statusCode: httpStatus.CREATED,
-      success: true,
-      message: 'User created successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'User created successfully',
+    data: result,
+  });
+});
 
-const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const result = await UserService.getAllUsersFromDB();
+const getAllUsers = catchAsync(async (req, res) => {
+  const result = await UserService.getAllUsersFromDB();
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'User retrived successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User retrived successfully',
+    data: result,
+  });
+});
 
-const getSingleUsers = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const id = req.params.id;
-    const result = await UserService.getSingleUsersFromDB(id);
+const getSingleUsers = catchAsync(async (req, res) => {
+  const id = req.params.id;
+  const result = await UserService.getSingleUsersFromDB(id);
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Single User retrived successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Single User retrived successfully',
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createUser,
