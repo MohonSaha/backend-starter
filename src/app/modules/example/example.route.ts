d708e0e4f951b9d7306d6c8a24b425f845c70abf
MohonSaha <practicewithmohon@gmail.com>
2024-01-05 17:24:57 +0600

    implement validateRequest middleware

--- src/app/modules/example/example.route.ts
+++ src/app/modules/example/example.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { UserControllers } from './example.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { UserValidations } from './example.validation';
 
 const router = express.Router();
 
-router.post('/create-user', UserControllers.createUser);
+router.post(
+  '/create-user',
+  validateRequest(UserValidations.createUserValidationSchema),
+  UserControllers.createUser
+);
 router.get('/', UserControllers.getAllUsers);
 router.get('/:id', UserControllers.getSingleUsers);
 
--- src/app/modules/example/example.validation.ts
+++ src/app/modules/example/example.validation.ts
@@ -1,15 +1,17 @@
 import { z } from 'zod';
 
-const userValidationSchema = z.object({
-  name: z.string({
-    required_error: 'Name is required',
-    invalid_type_error: 'Name must be a string',
+const createUserValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: 'Name is required',
+      invalid_type_error: 'Name must be a string',
+    }),
+    email: z.string(),
+    avatar: z.string(),
+    role: z.enum(['user', 'admin']),
   }),
-  email: z.string(),
-  avatar: z.string(),
-  role: z.enum(['user', 'admin']),
 });
 
 export const UserValidations = {
-  userValidationSchema,
+  createUserValidationSchema,
 };
