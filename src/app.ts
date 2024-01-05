1720fbcb7f09eb4a13d2afcf2c37389700bd6b4d
MohonSaha <practicewithmohon@gmail.com>
2024-01-05 11:20:18 +0600

    create index route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { UserRoutes } from './app/modules/example/example.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 const app: Application = express();
 
 // parser
@@ -10,11 +10,13 @@ app.use(express.json());
 app.use(cors());
 
 // Application routes
-app.use('/api/users', UserRoutes);
+app.use('/api', router);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+const test = (req: Request, res: Response) => {
+  res.send('Server is running ....');
+};
+
+app.use('/', test);
 
 // globalErrorHandlerMiddleware
 app.use(globalErrorHandler);
